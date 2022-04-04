e8963aaa1c145d6aba1d587101e29757dfaaac9d
rohitkumar.datchanamourty <rdatchane.19@gmail.com>
2022-04-04 22:51:23 +0200

    code commenting

--- main.py
+++ main.py
@@ -10,9 +10,10 @@ from youtube_transcript_api.formatters import Formatter
 from youtube_transcript_api.formatters import TextFormatter
 
 
+#Reading OpenAI API key stored in openai_key.txt in the same folder
 with open('openai_key.txt') as f:
 
-    openai.api_key = f.readlines()[0    ]
+    openai.api_key = f.readlines()[0]
     print("oeeeeeee")
     print(type(openai.api_key[0]))
 
@@ -34,9 +35,6 @@ def summarize(text):
         presence_penalty=0.0
         )
 
-
-        #response = response["choices"]["text"]
-        #response=json.stringify(response['choices'][0]['text'])
         st.markdown(response['choices'][0]['text'])
         print(response)
     
@@ -44,35 +42,29 @@ def summarize(text):
 
     
 
-#Download the subtitles of the given youtube video
+#Download the subtitles of the provided youtube video
 def getSubtitles(videoLink):
     videoID=videoLink[17:]
     transcript=YouTubeTranscriptApi.get_transcript(videoID)
     st.header("Video to summarize")
     download_thumbnail(video_id=videoID)
 
-
-
     return transcript
 
 
-def convert(transcript):
-
-    formatter = TextFormatter()
-    text_formatted= formatter.format_transcript(transcript) 
-
-    #st.markdown(text_formatted)
-    return text_formatted   
-
-def video_summarizer(video_link):
-    #summarize(convert(getSubtitles(video_link)))
-    summarize(convert(getSubtitles(video_link)))
-
-
+#Download thumnbail image for provided youtube video
 def download_thumbnail(video_id):
     thumbnailurl= 'https://img.youtube.com/vi/'+video_id + '/maxresdefault.jpg'
     thumbnail=wget.download(thumbnailurl)
     st.image(thumbnail)
+
+
+#Convert the downloaded transcript to a plain text format
+def convert(transcript):
+    formatter = TextFormatter()
+    text_formatted= formatter.format_transcript(transcript) 
+
+    return text_formatted   
 
 
 if __name__=="__main__":
